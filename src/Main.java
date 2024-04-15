6ff1b71cb92796ad89520704afa52300610749b3
katerina <katerina_panina1@mail.ru>
2024-04-15 20:35:13 +0300

    set default state of smart speaker

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 
 public class Main {
     public static void main(String[] args) {
+        SmartSpeaker speaker = new SmartSpeaker(new BacklightCommand(Backlight.on),
+                new MuteCommand(false),new OutputChannelDelayCommand(0), new VolumeCommand(100));
+
+        speaker.activate();
+
 
     }
 }
