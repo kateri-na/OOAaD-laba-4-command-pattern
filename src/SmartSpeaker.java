2121bba6215b7b246c4345c7f16bdc78eb8d16b5
katerina <katerina_panina1@mail.ru>
2024-04-15 20:34:58 +0300

    add Smart Speaker class

---
+++ src/SmartSpeaker.java
@@ -0,0 +1,32 @@
+public class SmartSpeaker {
+    private Connectivity backlight;
+    private Connectivity mute;
+    private Connectivity outputChannel;
+    private Connectivity volume;
+
+    public SmartSpeaker(Connectivity backlight, Connectivity mute, Connectivity outputChannel, Connectivity volume) {
+        this.backlight = backlight;
+        this.mute = mute;
+        this.outputChannel = outputChannel;
+        this.volume = volume;
+    }
+
+    public void activate(){
+        backlight.execute();
+        mute.execute();
+        outputChannel.execute();
+        volume.execute();
+    }
+    public void setBacklight(Connectivity backlight) {
+        this.backlight = backlight;
+    }
+    public void setMute(Connectivity mute) {
+        this.mute = mute;
+    }
+    public void setOutputChannel(Connectivity outputChannel) {
+        this.outputChannel = outputChannel;
+    }
+    public void setVolume(Connectivity volume) {
+        this.volume = volume;
+    }
+}
